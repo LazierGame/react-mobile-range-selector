1cc422e01d575fb76235647342afbf882127de84
ShianWang <984292420@qq.com>
2020-12-17 10:37:13 +0800

    feat: app 添加必要数据

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import { ExampleComponent } from 'test'
-import 'test/dist/index.css'
+import { ExampleComponent } from 'react-time-range-selector'
+import 'react-time-range-selector/dist/index.css'
 
 const App = () => {
   return <ExampleComponent text="Create React Library Example 😄" />
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react'
+import { DndProvider } from 'react-dnd'
+import { TouchBackend } from 'react-dnd-touch-backend'
 import styles from './styles.module.css'
+
 
 interface Props {
   text: string
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ExampleComponent = ({text}: Props) => {
+  return (
+    <DndProvider backend={TouchBackend}>
+      <div className={styles.test}>Example Component: {text}</div>
+    </DndProvider>
+  )
 }

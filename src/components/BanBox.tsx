e66ca6c040076d077c096f5f06e88c05371dbcb6
ShianWang <984292420@qq.com>
2021-01-06 00:14:26 +0800

    feat: 添加禁用的区域块

---
+++ src/components/BanBox.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface BanBoxProps {
+
+}
+
+/**
+ * 禁用的区域
+ * @constructor
+ */
+function BanBox(props: BanBoxProps) {
+  console.log(props)
+  return <>
+  </>
+}
+
+export default BanBox

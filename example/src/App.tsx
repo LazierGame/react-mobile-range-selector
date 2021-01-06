ce11453634497adba85b035475d5b1dde51630a0
ShianWang <984292420@qq.com>
2021-01-07 00:01:50 +0800

    feat: 修改 0.0.6

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TimeRangeSelector  from 'react-time-range-selector'
 import 'react-time-range-selector/dist/index.css'
+import { TimeRange } from "../../src/interfaces";
 
 const App = () => {
-  return <TimeRangeSelector value={[9,10]}  />
+  const [state, setState] = useState([9, 10] as TimeRange)
+  return <div>
+    <TimeRangeSelector value={state}  />
+    <button onClick={() => setState(prevState => [prevState[0], prevState[1] + 10])}> +</button>
+  </div>
 }
 
 export default App

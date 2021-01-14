ae3444e7c4fe76a40562382f9cfd7fe6591ee91e
ShianWang <984292420@qq.com>
2021-01-14 23:17:18 +0800

    bug, 单个页面一个 context 中拖拽，其他context 也会有拖拽效果

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -9,9 +9,16 @@ const App = () => {
   const handleChange = (value: any) => {
     setState(value)
   }
+
+  const [state2, setState2] = useState([1, 2] as TimeRange)
+
+  const handleChange2 = (value: any) => {
+    setState2(value)
+  }
   return <div>
     <TimeRangeSelector initialScrollIndex={(new Date()).getHours()} isSnapToGrid={false} snap={0.25} range='day' value={state} onChange={handleChange}  />
     <button onClick={() => setState(prevState => [prevState[0], prevState[1] + 1])}> +</button>
+    <TimeRangeSelector initialScrollIndex={(new Date()).getHours()} isSnapToGrid={false} snap={0.25} range='day' value={state2} onChange={handleChange2}  />
   </div>
 }
 

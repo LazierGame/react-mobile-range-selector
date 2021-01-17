c0649e272ea16a8cbfe4c834a6a5454962905cbc
ShianWang <984292420@qq.com>
2021-01-17 18:07:26 +0800

    feat: 测试 scrollLeft

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import TimeRangeSelector  from 'react-time-range-selector'
+import TimeRangeSelector from 'react-time-range-selector'
 import 'react-time-range-selector/dist/index.css'
 import { TimeRange } from "../../src/interfaces";
 
@@ -30,7 +30,7 @@ const App = () => {
       onScrollLeftChange={handleScrollLeftChange}
       onChange={handleChange}
     />
-    <button onClick={() => setState(prevState => [prevState[0], prevState[1] + 1])}> +</button>
+    <button onClick={() => setScrollLeft(prevState => prevState + 100)}> +</button>
     {/*<TimeRangeSelector initialScrollIndex={(new Date()).getHours()} isSnapToGrid={false} snap={0.25} range='day' value={state2} onChange={handleChange2}  />*/}
   </div>
 }

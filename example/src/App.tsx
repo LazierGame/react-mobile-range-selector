746edca51143c817f4d5b6d21d0e58ba3cee009a
ShianWang <984292420@qq.com>
2021-01-19 23:39:34 +0800

    feat: 添加 storybook/react

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,14 +5,20 @@ import { TimeRange } from "../../src/interfaces";
 
 const App = () => {
   const [state, setState] = useState([9, 10] as TimeRange)
+  const handleChange = (value: any) => {
+    setState(value)
+  }
+
+  const [state2, setState2] = useState([9, 10] as TimeRange)
+  const handleChange2 = (value: any) => {
+    setState2(value)
+  }
 
   const hour: number = (new Date()).getHours()
 
   const [scrollLeft, setScrollLeft] = useState(hour * 100)
 
-  const handleChange = (value: any) => {
-    setState(value)
-  }
+
 
   const handleScrollLeftChange = (value: number) => {
     setScrollLeft(value)
@@ -30,7 +36,7 @@ const App = () => {
       onChange={handleChange}
     />
     <button onClick={() => setScrollLeft(prevState => prevState + 100)}> +</button>
-    {/*<TimeRangeSelector initialScrollIndex={(new Date()).getHours()} isSnapToGrid={false} snap={0.25} range='day' value={state2} onChange={handleChange2}  />*/}
+    <TimeRangeSelector initialScrollIndex={(new Date()).getHours()} isSnapToGrid={false} snap={0.25} range='day' value={state2} onChange={handleChange2}  />
   </div>
 }
 
